3b3e24c1dd4276a77939e60e36309a4fb6efcc98
dstanchevici <distanchev@gmail.com>
2023-03-20 13:46:32 -0400

    3.22

---
+++ AntEater3.java
@@ -0,0 +1,103 @@
+
+import java.util.*;
+
+public class AntEater3 {
+
+    // A list of ants to hunt down.
+    static LinkedList<Coords> antQueue = new LinkedList<Coords>();
+
+    static Coords antEater;    // The current position of the anteater.
+    static Coords nextAnt;     // The position of the current ant being chased.
+
+    public static void main (String[] argv)
+    {
+	DrawTool.setXYRange (0,100, 0,100);
+	DrawTool.display ();
+
+        // This is where we'll start the anteater.
+        antEater = new Coords ();
+	antEater.x = 0;
+	antEater.y = 0;
+	
+	DrawTool.startAnimationMode ();
+
+	makeAntLocations (5);
+	nextAnt = antQueue.removeFirst();	
+	while (nextAnt != null) {
+	    huntNextAnt ();
+	    draw ();
+	    DrawTool.animationPause (500);
+	}
+	DrawTool.endAnimationMode ();
+    }
+
+    static void draw ()
+    {
+	// Draw ants as black dots.
+	DrawTool.setCircleColor ("black");
+
+	// nextAnt is not inside antQueue,
+	// so it needs to be drawn separately.
+	if (nextAnt != null) {
+	    DrawTool.drawCircle (nextAnt.x, nextAnt.y, 0.5);
+	}
+	
+	for (Coords c: antQueue) {
+	    DrawTool.drawCircle (c.x, c.y, 0.5);
+	}
+
+	// Draw anteater as red dot.
+	DrawTool.setCircleColor ("red");
+	DrawTool.drawCircle (antEater.x, antEater.y, 2);
+	
+    }
+
+    static void makeAntLocations (int numAnts)
+    {
+	for (int i=0; i<numAnts; i++) {
+	    Coords c = new Coords ();
+	    c.x = random (0,100);
+	    c.y = random (0,100);
+	    System.out.printf ("Ant located at: x=%6.1f y=%6.1f \n", c.x, c.y);
+	    antQueue.add (c);
+	}
+    }
+
+    static void huntNextAnt ()
+    {
+	// Check distance to current ant.
+	if ( distance(nextAnt, antEater) < 10 ) {
+	    // Eat the ant if the anteater is close enough.
+	    System.out.println ("Anteater: ate ant at x=" + nextAnt.x + " y=" + nextAnt.y);
+	    if (! antQueue.isEmpty()) {
+		nextAnt = antQueue.removeFirst();
+	    }
+	    else {
+		nextAnt = null;
+	    }
+	}
+	else {
+	    // Otherwise, step towards ant. This is the "trig math"
+	    // to find the direction and step in that direction.
+	    double theta = Math.atan2 ((nextAnt.y - antEater.y), (nextAnt.x - antEater.x));
+	    double stepsize = 10.0;
+	    antEater.x = antEater.x + stepsize*Math.cos(theta);
+	    antEater.y = antEater.y + stepsize*Math.sin(theta);
+	    System.out.println ("Anteater: moved to x=" + antEater.x + " y=" + antEater.y);
+        }
+    }
+
+    static double random (double x, double y)
+    {
+	Random rand = new Random ();
+	double z = (y-x) * rand.nextDouble();
+	return x + z;
+    }
+
+    static double distance (Coords p, Coords q)
+    {
+        double distSq = (p.x-q.x)*(p.x-q.x) + (p.y-q.y)*(p.y-q.y);
+        return Math.sqrt (distSq);
+    }
+
+}
